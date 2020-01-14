1617ca3d99d4d346c6ef8956f15a44de040fb362
thericktastic <love@The-RT-MBP.attlocal.net>
2020-01-14 18:57:24 -0500

    Completed MVP

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import "../App.css";
 
 const UserCard = props => {
     console.log("This is props: ", props)
     return (
-        <div>
-        <p>{props.login}</p>
+        <div className="card">
+        <img className="prof-pic" 
+        src={props.avatar} 
+        alt="profile picture"/>
+        <p>Secret Identity:<br />{props.login}</p>
         </div>
     )
 }
 
 export default UserCard;
+
+// <p>Cover: {props.name}</p>
+// <p>Location: {props.location}</p>
+// <p>Mission Statement: {props.bio}</p>
